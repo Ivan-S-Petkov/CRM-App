b82ce71baf9653346019ccdd6660fb2f0f10f823
Ivan Petkov <ivan.slv.petkov@gmail.com>
2023-11-25 14:47:09 +0200

    added Constats class

---
+++ src/com/crm/app/data/Constants.java
@@ -0,0 +1,7 @@
+package com.crm.app.data;
+
+public final class Constants {
+    public static final String CSV_DATA = "companies.csv";
+    public static final String JSON_DATA = "companies.json";
+
+}
